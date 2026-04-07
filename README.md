b842d2ab4cb0fda325a8bf83dd07effcf2774c50
rog <pessoal.rog@gmail.com>
2026-04-07 08:34:48 -0300

    docs(README): update readme
    improve docs

--- README.md
+++ README.md
@@ -1,11 +1,62 @@
-# rog commit
-A a commit cli made with [gum](https://github.com/charmbracelet/gum?tab=readme-ov-file#customization)
+#  ROG commit
+A commit cli made with [charm gum](https://github.com/charmbracelet/gum)
 
+## prerequisites
+git & [charm gum](https://github.com/charmbracelet/gum)
+
+## Instalation
+clone this repo:
+
+```sh
+gh repo clone https://github.com/rogcmd/rogcommit.git
+```
+
+## How to use
+
+### Executable: 
+use chmod to execute the file
+```sh
+chmod +x commit.sh
+
+# then execute the sript 
+./commit.sh
+```
+
+### With alias! 
+create an alias to execute the file anyware
+```sh title:config.fish
+# Fish example
+alias commit = "sh path/to/file/commit.sh"
+
+```
+
+## Styling
+u can customize the colors  to your 
+liking changing the eviroment variables! 
+
+```sh title:config.fish
+# GUM - Catppuccin Machiato
+set -gx GUM_INPUT_PROMPT "❯ "
+set -gx GUM_INPUT_HEADER_FOREGROUND "#94e2d5" # teal
+set -gx GUM_INPUT_PROMPT_FOREGROUND "#89b4fa" # blue
+set -gx GUM_INPUT_PLACEHOLDER_FOREGROUND "#6c7086" # overlay0
+
+set -gx GUM_CHOOSE_CURSOR "❯ "
+set -gx GUM_CHOOSE_CURSOR_FOREGROUND "#89b4fa" # blue
+set -gx GUM_CHOOSE_HEADER_FOREGROUND "#94e2d5" # teal
+set -gx GUM_CHOOSE_SELECTED_BACKGROUND "a6e3a1" # green
+
+set -gx GUM_WRITE_HEADER_FOREGROUND "#94e2d5" # Teal
+set -gx GUM_WRITE_PLACEHOLDER_FOREGROUND "#6c7086" # overlay0
+
+set -gx GUM_CONFIRM_PROMPT_FOREGROUND "#cdd6f4" # text
+set -gx GUM_CONFIRM_SELECTED_BACKGROUND "#313244" # surface0
+```
 
 TODO: 
-- [ ] verificar se a pessoa tem o gum instalado
 - [x] verificar se o git ta ativo
 - [ ] verificar se adicionou algum arquivo pra commit
 - [ ] opção para editar commit
 - [x] verificar se tem alguma mensagem de descrição vazia
-- [x] mostrar ultimos 3 commitsmmit
+- [x] mostrar ultimos 3 commits
+- [ ] verificar se o titulo ta em branco
--- commit.sh
+++ commit.sh
@@ -1,6 +1,5 @@
-#!bin/bash
-
-echo ""
+#!/usr/bin/env bash
+echo "" 
 
 gum style \
 --background "#cba6f7" \
@@ -12,64 +11,65 @@ gum style \
 echo ""
 if [ ! -d ".git" ]; then
     if gum confirm "not a git repository, git init?"; then 
-     git init && git branch -M "master" && git add . && echo ""
-     
+    git init && git branch -M "master" && git add . && echo ""
+      
     else
     gum style \
     --foreground "#f38ba8" \
     "You must be on a git repo"
-     
     fi 
 fi
 
 if [ -d ".git" ]; then
-
-TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
-SCOPE=$(gum input --header "scope: " --placeholder "scope of the commit")
-TITLE=$(gum write --header "$TYPE($SCOPE):" --placeholder "title of the commit")
-DESCRIPTION=$(gum write --header "Description" --placeholder "description of the commit")
-
-COMMIT="$TYPE($SCOPE): $TITLE"
-
-gum style \
-"Final commit:"\
-
-if [ -n "$DESCRIPTION" ]; then
-    FINAL_TEXT="$COMMIT
-
+    TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
+    SCOPE=$(gum input --header "scope: " --placeholder "scope of the commit")
+    TITLE=$(gum write --header "$TYPE($SCOPE):" --placeholder "title of the commit")
+    DESCRIPTION=$(gum write --header "Description" --placeholder "description of the commit")
+    
+    COMMIT="$TYPE($SCOPE): $TITLE"
+    clear
+    
+    gum style \
+    "Final commit:"\
+    
+    if [ -n "$DESCRIPTION" ]; then
+      FINAL_TEXT="$COMMIT
+      
 $DESCRIPTION"
-else
-    FINAL_TEXT="$COMMIT"
-fi
-
-gum style \
---foreground "#cba6f7" \
---border rounded \
---padding "1 1" \
---border-foreground "#89b4fa" \
---italic \
-"$FINAL_TEXT"
-
-echo ""
-
-# opcao para editar commit
-
-if gum confirm "commit changes?";
-then
-gum style \
---foreground "#a6e3a1" \
---italic \
-"operation succed"\
-&& git commit -m "$FINAL_TEXT"
-echo ""
-
-if gum confirm "show last 3 logs?"; then
-git log -3 --oneline
-fi
-else
-gum style \
---foreground "#f38ba8" \
-"cancelled operation"
-fi
-
+      else
+        FINAL_TEXT="$COMMIT"
+    fi
+    
+    gum style \
+    --foreground "#cba6f7" \
+    --border rounded \
+    --padding "1 1" \
+    --border-foreground "#89b4fa" \
+    --italic \
+    "$FINAL_TEXT"
+    
+    echo ""
+    
+    # opcao para editar commit
+    
+    if gum confirm "commit changes?";then
+      gum style \
+      --foreground "#94e2d5" \
+      "operation succed"\
+      && git commit -m "$FINAL_TEXT"
+      echo ""
+      
+      if gum confirm "push to a branch?"; then
+        git push
+        echo ""
+      fi
+      
+      if gum confirm "show last 3 logs?"; then
+        git log -3 --oneline
+      fi
+      else
+        gum style \
+        --foreground "#f38ba8" \
+        "cancelled operation"
+    fi
 fi
